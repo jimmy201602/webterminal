ce9482fdf35f45e43d1c0abef126d66ed1eb1014
Jimmy <zhengge2012@gmail.com>
2017-09-22 09:08:14 +0800

    add admin.py

---
+++ webterminal/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from webterminal.models import ServerInfor,ServerGroup,Credential
+
+admin.site.register(ServerInfor)
+admin.site.register(ServerGroup)
+admin.site.register(Credential)
