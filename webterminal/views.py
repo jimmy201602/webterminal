2bfdfe1998bf635d7c2bf055a392cd15d392dac9
jimmy <zhengge2012@gmail.com>
2017-09-23 14:51:17 +0800

    add commands workflow validation

--- webterminal/views.py
+++ webterminal/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import View
 from django.shortcuts import render_to_response,HttpResponse
 from django.http import JsonResponse
-from webterminal.models import ServerGroup
+from webterminal.models import ServerGroup,CommandsSequence
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect,csrf_exempt
 try:
@@ -9,7 +9,9 @@ try:
 except ImportError:
     import json
 from django.contrib import messages as message
-    
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
+
 class Index(View):
     def get(self,request):
         server_groups=ServerGroup.objects.all()
@@ -24,8 +26,20 @@ class Commands(View):
         if request.is_ajax():
             try:
                 data = json.loads(request.body)
+                obj = CommandsSequence.objects.create(name=data['name'],commands=data['commands'])
+                for group in data['group']:
+                    obj.group.add(ServerGroup.objects.get(name=group))
+                obj.save()
                 return JsonResponse({'status':True,'message':'%s create success!' %(str(data.get('name',None)))})
+            except ObjectDoesNotExist:
+                return JsonResponse({'status':False,'message':'Please input a valid group name!' })
+            except IntegrityError:
+                return JsonResponse({'status':False,'message':'Task name:%s already exist,Please use another name instead!' %(data['name']) })
+            except KeyError:
+                return JsonResponse({'status':False,'message':"Invalid parameter,Please report it to the adminstrator!" })
             except Exception,e:
+                import traceback
+                print traceback.print_exc()
                 return JsonResponse({'status':False,'message':'Some error happend! Please report it to the adminstrator! Error info:%s' %(str(e)) })
         else:
             pass
