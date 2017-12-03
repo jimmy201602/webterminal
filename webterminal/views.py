8d925764456bf2bf861310309d5eac39b26a5ac2
jimmy201602 <zhengge2012@gmail.com>
2017-12-03 10:37:13 +0800

    add terminal kill api

--- webterminal/views.py
+++ webterminal/views.py
@@ -18,6 +18,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.serializers import serialize
 from webterminal.settings import MEDIA_URL
+from django.utils.timezone import now
+from webterminal.interactive import get_redis_instance
 
 class Index(LoginRequiredMixin,View):
     def get(self,request):
@@ -202,4 +204,26 @@ class SshLogPlay(LoginRequiredMixin,DetailView):
         context = super(SshLogPlay, self).get_context_data(**kwargs)
         objects = kwargs['object']
         context['logpath'] = '{0}{1}-{2}-{3}/{4}.json'.format(MEDIA_URL,objects.start_time.year,objects.start_time.month,objects.start_time.day,objects.log)
-        return context     
+        return context
+
+class SshTerminalKill(LoginRequiredMixin,View):
+    
+    def post(self,request):
+        if request.is_ajax():
+            channel_name = request.POST.get('channel_name',None)
+            try:
+                data = SshLog.objects.get(channel=channel_name)
+                if data.is_finished:
+                    return JsonResponse({'status':False,'message':'Ssh terminal does not exist!'})
+                else:
+                    data.end_time = now()
+                    data.is_finished = True
+                    data.save()
+                    
+                    queue = get_redis_instance()
+                    redis_channel = queue.pubsub()
+                    queue.publish(channel_name, json.dumps(['close'])) 
+                    
+                    return JsonResponse({'status':True,'message':'Terminal has been killed !'})
+            except ObjectDoesNotExist:
+                return JsonResponse({'status':False,'message':'Request object does not exist!'})
