a3db970a30d33c217854c54d86a5b280e2baec25
jimmy201602 <zhengge2012@gmail.com>
2018-02-28 22:09:31 +0800

    modify installation script

--- install.sh
+++ install.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 set -e
-function centos6install() {
+function centosinstall() {
     os=`rpm -q centos-release|cut -d- -f3`
 	if [[ $os -eq 6 ]]; then
 				yum clean all
-				yum install centos-release-scl -y
-				yum install python27 gcc -y
+				yum install centos-release-scl epel-release -y
+				yum install python27 gcc python27-python-pip python27-python-setuptools python27-python-devel -y
 				scl enable python27 bash
-				pip install setuptools -U
-				pip install pip -U
-				pip install -r requirements.txt
+				yum install redis -y
+				pip install setuptools pip -U -i https://pypi.douban.com/simple/
 	elif [[ $os -eq 7 ]]; then
-			apt-get install smartmontools dmidecode python-pip python-dev
+				yum clean all
+				yum install python-setuptools gcc python python-devel epel-release -y
+				yum install redis -y
+				easy_install pip
+				pip install setuptools pip -U -i https://pypi.douban.com/simple/
 	else
-		echo "your os version is not supported!"
+		echo "Your os version is not supported!"
 		exit 0
 	fi
 }
 
+function commoninstall() {
+    pip install -r requirements.txt
+}
+
 function install() {
     if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
-		    DISTRO='CentOS'
+	DISTRO='CentOS'
         PM='yum'
-		centos6install
+	centosinstall
     elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
         DISTRO='RHEL'
         PM='yum'
@@ -45,6 +52,7 @@ function install() {
         DISTRO='unknow'
     fi
     echo $DISTRO;
+    commoninstall
 }
 
 install
