eb893365e9d991ae09579c374801e4c71ddaa7ba
jimmy201602 <zhengge2012@gmail.com>
2018-03-14 22:42:01 +0800

    add guacamole support

--- webterminal/routing.py
+++ webterminal/routing.py
@@ -1,5 +1,6 @@
 from channels import route_class, route
 from webterminal.consumers import webterminal,CommandExecute,SshTerminalMonitor
+from guacamole.consumers import GuacamoleWebsocket
 
 # The channel routing defines what channels get handled by what consumers,
 # including optional matching on message attributes. In this example, we route
@@ -9,4 +10,5 @@ channel_routing = [
     route_class(webterminal,path = r'^/ws'),
     route_class(CommandExecute,path= r'^/execute'),
     route_class(SshTerminalMonitor,path= r'^/monitor/(?P<channel>\w+-\w+-\w+-\w+-\w+-\w+)'),
+    route_class(GuacamoleWebsocket,path = r'^/guacamole'),
 ]
