feb873a9b4a4d77d0f5115294ea034d683590f36
jimmy201602 <zhengge2012@gmail.com>
2018-03-25 17:42:34 +0800

    add models translation and add permission control

--- webterminal/models.py
+++ webterminal/models.py
@@ -7,14 +7,15 @@ except ImportError:
 from django.contrib.auth.models import User 
 import uuid
 from django.utils.text import slugify
+from django.utils.translation import gettext as _
 
 class ServerInfor(models.Model):
-    name = models.CharField(max_length=40,verbose_name='Server name',blank=False,unique=True)
-    hostname = models.CharField(max_length=40,verbose_name='Host name',blank=True)
-    ip = models.GenericIPAddressField(protocol='ipv4',blank=False)
-    createdatetime = models.DateTimeField(auto_now_add=True)
-    updatedatetime = models.DateTimeField(auto_created=True,auto_now=True)
-    credential = models.ForeignKey('Credential')
+    name = models.CharField(max_length=40,verbose_name=_('Server name'),blank=False,unique=True)
+    hostname = models.CharField(max_length=40,verbose_name=_('Host name'),blank=True)
+    ip = models.GenericIPAddressField(protocol='ipv4',verbose_name=_('ip'),blank=False)
+    createdatetime = models.DateTimeField(auto_now_add=True,verbose_name=_('Create time'))
+    updatedatetime = models.DateTimeField(auto_created=True,auto_now=True,verbose_name=_('Update time'))
+    credential = models.ForeignKey(_('Credential'))
     
     def __unicode__(self):
         return self.name
@@ -23,53 +24,53 @@ class ServerInfor(models.Model):
     
     class Meta:
         permissions = (
-            ("can_add", "Can add server"),
-            ("can_change", "Can change server info"),
-            ("can_delete", "Can delete server info"),
-            ("can_connect", "Can connect to server"),
-            ("can_view", "Can view server info"),
+            ("can_add", _("Can add server")),
+            ("can_change", _("Can change server info")),
+            ("can_delete", _("Can delete server info")),
+            ("can_connect", _("Can connect to server")),
+            ("can_view", _("Can view server info")),
         )
 
 class ServerGroup(models.Model):
-    name = models.CharField(max_length=40,verbose_name='Server group name',blank=False,unique=True)
-    servers = models.ManyToManyField(ServerInfor,related_name='servers')
-    createdatetime = models.DateTimeField(auto_now_add=True)
-    updatedatetime = models.DateTimeField(auto_created=True,auto_now=True)
+    name = models.CharField(max_length=40,verbose_name=_('Server group name'),blank=False,unique=True)
+    servers = models.ManyToManyField(ServerInfor,related_name='servers',verbose_name=_('Servers'))
+    createdatetime = models.DateTimeField(auto_now_add=True,verbose_name=_('Create time'))
+    updatedatetime = models.DateTimeField(auto_created=True,auto_now=True,verbose_name=_('Update time'))
     
     def __unicode__(self):
         return self.name
 
     class Meta:
         permissions = (
-            ("can_add", "Can add group"),
-            ("can_change", "Can change group info"),
-            ("can_delete", "Can delete group info"),
-            ("can_view", "Can view group info"),
+            ("can_add", _("Can add group")),
+            ("can_change", _("Can change group info")),
+            ("can_delete", _("Can delete group info")),
+            ("can_view", _("Can view group info")),
         )
 
 class Credential(models.Model):
     protocol_choices = (
-            ('ssh-password','ssh-password'),
-            ('ssh-key','ssh-key'),
-            ('vnc','vnc'),
-            ('rdp','rdp'),
-            ('telnet','telnet')
+            ('ssh-password',_('ssh-password')),
+            ('ssh-key',_('ssh-key')),
+            ('vnc',_('vnc')),
+            ('rdp',_('rdp')),
+            ('telnet',_('telnet'))
         )
 
-    name = models.CharField(max_length=40,verbose_name='Credential name',blank=False,unique=True)
-    username = models.CharField(max_length=40,verbose_name='Auth user name',blank=False)
-    port = models.PositiveIntegerField(default=22,blank=False)
-    method = models.CharField(max_length=40,choices=(('password','password'),('key','key')),blank=False,default='password')
-    key = models.TextField(blank=True)
-    password = models.CharField(max_length=40,blank=True)
-    proxy = models.BooleanField(default=False)
-    proxyserverip = models.GenericIPAddressField(protocol='ipv4',null=True, blank=True)
-    proxyport = models.PositiveIntegerField(blank=True,null=True)
-    proxypassword = models.CharField(max_length=40,verbose_name='Proxy password',blank=True)
-    protocol = models.CharField(max_length=40,default='ssh-password', choices=protocol_choices)
-    width = models.PositiveIntegerField(verbose_name='width',default=1024)
-    height = models.PositiveIntegerField(verbose_name='height',default=768)
-    dpi = models.PositiveIntegerField(verbose_name='dpi',default=96)
+    name = models.CharField(max_length=40,verbose_name=_('Credential name'),blank=False,unique=True)
+    username = models.CharField(max_length=40,verbose_name=_('Auth user name'),blank=False)
+    port = models.PositiveIntegerField(default=22,blank=False,verbose_name=_('Port'))
+    method = models.CharField(max_length=40,choices=(('password',_('password')),('key',_('key'))),blank=False,default='password',verbose_name=_('Method'))
+    key = models.TextField(blank=True,verbose_name=_('Key'))
+    password = models.CharField(max_length=40,blank=True,verbose_name=_('Password'))
+    proxy = models.BooleanField(default=False,verbose_name=_('Proxy'))
+    proxyserverip = models.GenericIPAddressField(protocol='ipv4',null=True, blank=True,verbose_name=_('Proxy ip'))
+    proxyport = models.PositiveIntegerField(blank=True,null=True,verbose_name=_('Proxy port'))
+    proxypassword = models.CharField(max_length=40,verbose_name=_('Proxy password'),blank=True)
+    protocol = models.CharField(max_length=40,default='ssh-password', choices=protocol_choices,verbose_name=_('Protocol'))
+    width = models.PositiveIntegerField(verbose_name=_('width'),default=1024)
+    height = models.PositiveIntegerField(verbose_name=_('height'),default=768)
+    dpi = models.PositiveIntegerField(verbose_name=_('dpi'),default=96)
 
     def __unicode__(self):
         return self.name    
@@ -77,29 +78,29 @@ class Credential(models.Model):
     def clean(self):
         if self.protocol == 'ssh-password' or self.protocol == 'ssh-key':
             if self.method == 'password' and len(self.password) == 0:
-                raise ValidationError('If you choose password auth method,You must set password!')
+                raise ValidationError(_('If you choose password auth method,You must set password!'))
             if self.method == 'password' and len(self.key) >0:
-                raise ValidationError('If you choose password auth method,You must make key field for blank!')
+                raise ValidationError(_('If you choose password auth method,You must make key field for blank!'))
             if self.method == 'key' and len(self.key) == 0:
-                raise ValidationError('If you choose key auth method,You must fill in key field!')
+                raise ValidationError(_('If you choose key auth method,You must fill in key field!'))
             if self.method == 'key' and len(self.password) >0:
-                raise ValidationError('If you choose key auth method,You must make password field for blank!')  
+                raise ValidationError(_('If you choose key auth method,You must make password field for blank!'))
             if self.proxy:
                 if self.proxyserverip is None or self.proxyport is None:
-                    raise ValidationError('If you choose auth proxy,You must fill in proxyserverip and proxyport field !')
+                    raise ValidationError(_('If you choose auth proxy,You must fill in proxyserverip and proxyport field !'))
 
     class Meta:
         permissions = (
-            ("can_add", "Can add credential"),
-            ("can_change", "Can change credential info"),
-            ("can_delete", "Can delete credential info"),
-            ("can_view", "Can view credential info"),
+            ("can_add", _("Can add credential")),
+            ("can_change", _("Can change credential info")),
+            ("can_delete", _("Can delete credential info")),
+            ("can_view", _("Can view credential info")),
         )
 
 class CommandsSequence(models.Model):
-    name = models.CharField(max_length=40,verbose_name='Task name',blank=False,unique=True)
-    commands = models.TextField(verbose_name='Task commands',blank=False)
-    group = models.ManyToManyField(ServerGroup,verbose_name='Server group you want to execute')
+    name = models.CharField(max_length=40,verbose_name=_('Task name'),blank=False,unique=True)
+    commands = models.TextField(verbose_name=_('Task commands'),blank=False)
+    group = models.ManyToManyField(ServerGroup,verbose_name=_('Server group you want to execute'))
     
     def __unicode__(self):
         return self.name
@@ -108,7 +109,7 @@ class CommandsSequence(models.Model):
         try:
             json.dumps(self.commands)
         except Exception:
-            raise ValidationError('Commands sequence is not valid json type')
+            raise ValidationError(_('Commands sequence is not valid json type'))
         
     def save(self, *args, **kwargs):
         if isinstance(self.commands,(list)):
@@ -117,30 +118,30 @@ class CommandsSequence(models.Model):
 
     class Meta:
         permissions = (
-            ("can_add", "Can add commands"),
-            ("can_change", "Can change commands info"),
-            ("can_delete", "Can delete commands info"),
-            ("can_view", "Can view commands info"),
+            ("can_add", _("Can add commands")),
+            ("can_change", _("Can change commands info")),
+            ("can_delete", _("Can delete commands info")),
+            ("can_view", _("Can view commands info")),
         )
 
 class Log(models.Model):
-    server = models.ForeignKey(ServerInfor)
-    channel = models.CharField(max_length=100,verbose_name='Channel name',blank=False,unique=True,editable=False)
-    log = models.UUIDField(max_length=100,default=uuid.uuid4,verbose_name='Log name',blank=False,unique=True,editable=False)
-    start_time = models.DateTimeField(auto_now_add=True)
-    end_time = models.DateTimeField(auto_created=True,auto_now=True)
-    is_finished = models.BooleanField(default=False)
-    user = models.ForeignKey(User)
-    width = models.PositiveIntegerField(default=90)
-    height = models.PositiveIntegerField(default=40)
+    server = models.ForeignKey(ServerInfor,verbose_name=_('Server'))
+    channel = models.CharField(max_length=100,verbose_name=_('Channel name'),blank=False,unique=True,editable=False)
+    log = models.UUIDField(max_length=100,default=uuid.uuid4,verbose_name=_('Log name'),blank=False,unique=True,editable=False)
+    start_time = models.DateTimeField(auto_now_add=True,verbose_name=_('Start time'))
+    end_time = models.DateTimeField(auto_created=True,auto_now=True,verbose_name=_('End time'))
+    is_finished = models.BooleanField(default=False,verbose_name=_('Is finished'))
+    user = models.ForeignKey(User,verbose_name=_('User'))
+    width = models.PositiveIntegerField(default=90,verbose_name=_('Width'))
+    height = models.PositiveIntegerField(default=40,verbose_name=_('Height'))
 
     def __unicode__(self):
         return self.server.name
     
     class Meta:
         permissions = (
-            ("can_delete", "Can delete log info"),
-            ("can_view", "Can view log info"),
+            ("can_delete", _("Can delete log info")),
+            ("can_view", _("Can view log info")),
         )
         ordering = [
             ('-start_time')
