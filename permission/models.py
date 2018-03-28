9bdb5b9d9198d9149cc3ee8f19d593beafdf67d6
Jimmy <zhengge2012@gmail.com>
2018-03-28 13:05:06 +0800

    modify project permission control

--- permission/models.py
+++ permission/models.py
@@ -4,12 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 from webterminal.models import ServerGroup
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission as AuthPermission
 from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import gettext as _
 
 class Permission(models.Model):
-    user = models.ForeignKey(User,verbose_name=_('User'),related_name='permissionuser')
-    permissions = models.ManyToManyField(ContentType,verbose_name=_('Permissions'),related_name='permissions')
+    user = models.OneToOneField(User,verbose_name=_('User'),related_name='permissionuser')
+    permissions = models.ManyToManyField(AuthPermission,verbose_name=_('Permissions'),related_name='permission')
     groups = models.ManyToManyField(ServerGroup,verbose_name=_('Server group'))
     createdatetime = models.DateTimeField(auto_now_add=True,verbose_name=_('Create time'))
     updatedatetime = models.DateTimeField(auto_created=True,auto_now=True,verbose_name=_('Update time'))
