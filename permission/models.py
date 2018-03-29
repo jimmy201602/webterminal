c5f1930cc734a66b8077b25ccd16adf27a22004a
jimmy201602 <zhengge2012@gmail.com>
2018-03-29 21:32:04 +0800

    add permission control

--- permission/models.py
+++ permission/models.py
@@ -18,4 +18,14 @@ class Permission(models.Model):
     def __unicode__(self):
         return self.user
     
-    
+    class Meta:
+        permissions = (
+            ("can_add_user", _("Can add user")),
+            ("can_change_user", _("Can change user info")),
+            ("can_delete_user", _("Can delete user info")),
+            ("can_view_user", _("Can view user info")),
+            ("can_view_permissions", _("Can view user permissions")),
+            ("can_change_permissions", _("Can change user permissions")),
+            ("can_delete_permissions", _("Can revoke user permissions")),
+            ("can_add_permissions", _("Can add user permissions")),
+        )
