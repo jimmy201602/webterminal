60bd8c2cdd5ab8d64e707dcd075ef2a7977b4d88
jimmy201602 <zhengge2012@gmail.com>
2018-03-29 22:08:11 +0800

    add permission control

--- permission/views.py
+++ permission/views.py
@@ -8,11 +8,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy
 from permission.models import Permission
 from django.utils.translation import ugettext, ugettext_lazy as _
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import AccessMixin
 
-class UserRegister(LoginRequiredMixin,FormView):
+class UserRegister(PermissionRequiredMixin,LoginRequiredMixin,FormView):
     template_name = 'permission/userregister.html'
     form_class = RegisterForm
     success_url = reverse_lazy('userlist')
+    permission_required = 'permission.can_add_user'
+    raise_exception = True
 
     def form_valid(self, form):
         username=form.cleaned_data['user']
@@ -21,28 +25,35 @@ class UserRegister(LoginRequiredMixin,FormView):
         User.objects.create_user(username=username,email=email,password=password,is_active=True)
         return HttpResponseRedirect(self.get_success_url())
 
-class UserList(LoginRequiredMixin,ListView):
+class UserList(PermissionRequiredMixin,LoginRequiredMixin,ListView):
     template_name='permission/userlist.html'
     model=User
+    permission_required = 'permission.can_view_user'
+    raise_exception = True
 
-class UserDelete(LoginRequiredMixin,DeleteView):
+class UserDelete(PermissionRequiredMixin,LoginRequiredMixin,DeleteView):
     template_name='permission/userdelete.html'
     model=User
     success_url = reverse_lazy('userlist')
+    permission_required = 'permission.can_delete_user'
+    raise_exception = True
 
-class UserUpdate(LoginRequiredMixin,UpdateView):
+class UserUpdate(PermissionRequiredMixin,LoginRequiredMixin,UpdateView):
     template_name='permission/userupdate.html'
     model=User
     fields=['email']
     success_url = reverse_lazy('userlist')
+    permission_required = 'permission.can_change_user'
+    raise_exception = True
 
-
-class PermissionCreate(LoginRequiredMixin,CreateView):
+class PermissionCreate(PermissionRequiredMixin,LoginRequiredMixin,CreateView):
     model = Permission
     form_class = PermissionForm
     success_url = reverse_lazy('permissionlist')
     template_name = 'permission/permissioncreate.html'
-    
+    permission_required = 'permission.can_add_permissions'
+    raise_exception = True
+
     def get_context_data(self, **kwargs):
         context = super(PermissionCreate, self).get_context_data(**kwargs)
         context['title'] = _('Create Permission')
@@ -57,16 +68,20 @@ class PermissionCreate(LoginRequiredMixin,CreateView):
         self.object = form.save()
         return super(PermissionCreate, self).form_valid(form)
 
-class PermissionList(LoginRequiredMixin,ListView):
+class PermissionList(PermissionRequiredMixin,LoginRequiredMixin,ListView):
     model = Permission
     template_name = 'permission/permissionlist.html'
+    permission_required = 'permission.can_view_permissions'
+    raise_exception = True
 
-class PermissionUpdate(LoginRequiredMixin,UpdateView):
+class PermissionUpdate(PermissionRequiredMixin,LoginRequiredMixin,UpdateView):
     model = Permission
     form_class = PermissionForm
     success_url = reverse_lazy('permissionlist')
     template_name = 'permission/permissioncreate.html'
-    
+    permission_required = 'permission.can_change_permissions'
+    raise_exception = True
+
     def get_context_data(self, **kwargs):
         context = super(PermissionUpdate, self).get_context_data(**kwargs)
         context['title'] = _('Update Permission')
@@ -81,10 +96,12 @@ class PermissionUpdate(LoginRequiredMixin,UpdateView):
         self.object = form.save()
         return super(PermissionUpdate, self).form_valid(form)
 
-class PermissionDelete(LoginRequiredMixin,DeleteView):
+class PermissionDelete(PermissionRequiredMixin,LoginRequiredMixin,DeleteView):
     model = Permission
     success_url = reverse_lazy('permissionlist')
     template_name = 'permission/permissiondelete.html'
+    permission_required = 'permission.can_delete_permissions'
+    raise_exception = True
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
