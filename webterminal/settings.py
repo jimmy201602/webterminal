077777899132d0fc817b3a254eb3717497e6dc85
Jimmy <zhengge2012@gmail.com>
2018-04-03 13:02:12 +0800

    fix index page login bug

--- webterminal/settings.py
+++ webterminal/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, "static"),
+#]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Channels settings
 CHANNEL_LAYERS = {
     "default": {
