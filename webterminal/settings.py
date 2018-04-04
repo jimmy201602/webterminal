04981dca3ef820f8003c3caa636770c3f4318fa5
Jimmy <zhengge2012@gmail.com>
2018-04-04 14:11:43 +0800

    modify language code

--- webterminal/settings.py
+++ webterminal/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'zh-hans'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
