8378275836a83e20e8d57c12f3ffb66edfd1f488
Jimmy <zhengge2012@gmail.com>
2018-04-09 15:01:26 +0800

    add select2 theme

--- webterminal/admin.py
+++ webterminal/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from webterminal.models import ServerInfor,ServerGroup,Credential,CommandsSequence,Log
 
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
 admin.site.register(ServerInfor)
 admin.site.register(ServerGroup)
 admin.site.register(Credential)
 admin.site.register(CommandsSequence)
-admin.site.register(Log)
+admin.site.register(Log,LogAdmin)
