67df3e4c193be556f27b0d099627af85f4d09de7
jimmy201602 <zhengge2012@gmail.com>
2018-06-22 21:37:13 +0800

    update create user script to delete AnonymousUser user

--- createsuperuser.py
+++ createsuperuser.py
@@ -10,6 +10,10 @@ email = 'admin@admin.com'
 
 if User.objects.filter(username=username).count()==0:
     User.objects.create_superuser(username, email, password)
+    try:
+        User.objects.get(username='AnonymousUser').delete()
+    except Exception:
+        pass
     print('Superuser created.')
 else:
     print('Superuser creation skipped.')
