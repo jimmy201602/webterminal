ea7724b35ceb19c16e1cb2a82c1409bad0ecef33
jimmy201602 <zhengge2012@gmail.com>
2018-06-30 14:56:56 +0800

    add batch command execute function

--- webterminal/routing.py
+++ webterminal/routing.py
@@ -1,5 +1,5 @@
 from channels import route_class, route
-from webterminal.consumers import webterminal,CommandExecute,SshTerminalMonitor
+from webterminal.consumers import webterminal,CommandExecute,SshTerminalMonitor,BatchCommandExecute
 from guacamole.consumers import GuacamoleWebsocket,GuacamoleMonitor
 
 # The channel routing defines what channels get handled by what consumers,
@@ -12,4 +12,5 @@ channel_routing = [
     route_class(SshTerminalMonitor,path= r'^/monitor/(?P<channel>\w+-\w+-\w+-\w+-\w+-\w+)'),
     route_class(GuacamoleWebsocket,path = r'^/guacamole/(?P<id>[0-9]+)/'),
     route_class(GuacamoleMonitor,path = r'^/guacamolemonitor/(?P<id>[0-9]+)/'),
+    route_class(BatchCommandExecute,path = r'^/batchexecute/'),
 ]
