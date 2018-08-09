7aa98ac737adf27213061416f59ba7b5d7c4b351
jimmy201602 <zhengge2012@gmail.com>
2018-08-09 21:25:54 +0800

    fix route url bug

--- webterminal/routing.py
+++ webterminal/routing.py
@@ -9,7 +9,7 @@ from guacamole.consumers import GuacamoleWebsocket,GuacamoleMonitor
 channel_routing = [
     route_class(webterminal,path = r'^/ws'),
     route_class(CommandExecute,path= r'^/execute'),
-    route_class(SshTerminalMonitor,path= r'^/monitor/(?P<channel>\w+-\w+-\w+-\w+-\w+-\w+)'),
+    route_class(SshTerminalMonitor,path= r'^/monitor/(?P<channel>[\w-]+)'),
     route_class(GuacamoleWebsocket,path = r'^/guacamole/(?P<id>[0-9]+)/'),
     route_class(GuacamoleMonitor,path = r'^/guacamolemonitor/(?P<id>[0-9]+)/'),
     route_class(BatchCommandExecute,path = r'^/batchexecute/'),
