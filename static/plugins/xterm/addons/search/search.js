adb61937fb3f38e38b943245c3963dee74523529
Jimmy <zhengge2012@gmail.com>
2018-11-01 21:25:27 +0800

    update xterm.js version

--- static/plugins/xterm/addons/search/search.js
+++ static/plugins/xterm/addons/search/search.js
@@ -1,29 +1,31 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.search = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var nonWordCharacters = ' ~!@#$%^&*()+`-=[]{}|\;:"\',./<>?';
 var SearchHelper = (function () {
-    function SearchHelper(_terminal, _translateBufferLineToString) {
+    function SearchHelper(_terminal) {
         this._terminal = _terminal;
-        this._translateBufferLineToString = _translateBufferLineToString;
     }
-    SearchHelper.prototype.findNext = function (term) {
+    SearchHelper.prototype.findNext = function (term, searchOptions) {
         if (!term || term.length === 0) {
             return false;
         }
         var result;
-        var startRow = this._terminal.buffer.ydisp;
-        if (this._terminal.selectionManager.selectionEnd) {
-            startRow = this._terminal.selectionManager.selectionEnd[1];
+        var startRow = this._terminal._core.buffer.ydisp;
+        if (this._terminal._core.selectionManager.selectionEnd) {
+            if (this._terminal.getSelection().length !== 0) {
+                startRow = this._terminal._core.selectionManager.selectionEnd[1];
+            }
         }
-        for (var y = startRow + 1; y < this._terminal.buffer.ybase + this._terminal.rows; y++) {
-            result = this._findInLine(term, y);
+        for (var y = startRow + 1; y < this._terminal._core.buffer.ybase + this._terminal.rows; y++) {
+            result = this._findInLine(term, y, searchOptions);
             if (result) {
                 break;
             }
         }
         if (!result) {
             for (var y = 0; y < startRow; y++) {
-                result = this._findInLine(term, y);
+                result = this._findInLine(term, y, searchOptions);
                 if (result) {
                     break;
                 }
@@ -31,24 +33,26 @@ var SearchHelper = (function () {
         }
         return this._selectResult(result);
     };
-    SearchHelper.prototype.findPrevious = function (term) {
+    SearchHelper.prototype.findPrevious = function (term, searchOptions) {
         if (!term || term.length === 0) {
             return false;
         }
         var result;
-        var startRow = this._terminal.buffer.ydisp;
-        if (this._terminal.selectionManager.selectionStart) {
-            startRow = this._terminal.selectionManager.selectionStart[1];
+        var startRow = this._terminal._core.buffer.ydisp;
+        if (this._terminal._core.selectionManager.selectionStart) {
+            if (this._terminal.getSelection().length !== 0) {
+                startRow = this._terminal._core.selectionManager.selectionStart[1];
+            }
         }
         for (var y = startRow - 1; y >= 0; y--) {
-            result = this._findInLine(term, y);
+            result = this._findInLine(term, y, searchOptions);
             if (result) {
                 break;
             }
         }
         if (!result) {
-            for (var y = this._terminal.buffer.ybase + this._terminal.rows - 1; y > startRow; y--) {
-                result = this._findInLine(term, y);
+            for (var y = this._terminal._core.buffer.ybase + this._terminal.rows - 1; y > startRow; y--) {
+                result = this._findInLine(term, y, searchOptions);
                 if (result) {
                     break;
                 }
@@ -56,12 +60,50 @@ var SearchHelper = (function () {
         }
         return this._selectResult(result);
     };
-    SearchHelper.prototype._findInLine = function (term, y) {
-        var bufferLine = this._terminal.buffer.lines.get(y);
-        var lowerStringLine = this._translateBufferLineToString(bufferLine, true).toLowerCase();
-        var lowerTerm = term.toLowerCase();
-        var searchIndex = lowerStringLine.indexOf(lowerTerm);
+    SearchHelper.prototype._isWholeWord = function (searchIndex, line, term) {
+        return (((searchIndex === 0) || (nonWordCharacters.indexOf(line[searchIndex - 1]) !== -1)) &&
+            (((searchIndex + term.length) === line.length) || (nonWordCharacters.indexOf(line[searchIndex + term.length]) !== -1)));
+    };
+    SearchHelper.prototype._findInLine = function (term, y, searchOptions) {
+        if (searchOptions === void 0) { searchOptions = {}; }
+        if (this._terminal._core.buffer.lines.get(y).isWrapped) {
+            return;
+        }
+        var stringLine = this.translateBufferLineToStringWithWrap(y, true);
+        var searchStringLine = searchOptions.caseSensitive ? stringLine : stringLine.toLowerCase();
+        var searchTerm = searchOptions.caseSensitive ? term : term.toLowerCase();
+        var searchIndex = -1;
+        if (searchOptions.regex) {
+            var searchRegex = RegExp(searchTerm, 'g');
+            var foundTerm = searchRegex.exec(searchStringLine);
+            if (foundTerm && foundTerm[0].length > 0) {
+                searchIndex = searchRegex.lastIndex - foundTerm[0].length;
+                term = foundTerm[0];
+            }
+        }
+        else {
+            searchIndex = searchStringLine.indexOf(searchTerm);
+        }
         if (searchIndex >= 0) {
+            if (searchIndex >= this._terminal.cols) {
+                y += Math.floor(searchIndex / this._terminal.cols);
+                searchIndex = searchIndex % this._terminal.cols;
+            }
+            if (searchOptions.wholeWord && !this._isWholeWord(searchIndex, searchStringLine, term)) {
+                return;
+            }
+            var line = this._terminal._core.buffer.lines.get(y);
+            for (var i = 0; i < searchIndex; i++) {
+                var charData = line.get(i);
+                var char = charData[1];
+                if (char.length > 1) {
+                    searchIndex -= char.length - 1;
+                }
+                var charWidth = charData[2];
+                if (charWidth === 0) {
+                    searchIndex++;
+                }
+            }
             return {
                 term: term,
                 col: searchIndex,
@@ -69,50 +111,61 @@ var SearchHelper = (function () {
             };
         }
     };
+    SearchHelper.prototype.translateBufferLineToStringWithWrap = function (lineIndex, trimRight) {
+        var lineString = '';
+        var lineWrapsToNext;
+        do {
+            var nextLine = this._terminal._core.buffer.lines.get(lineIndex + 1);
+            lineWrapsToNext = nextLine ? nextLine.isWrapped : false;
+            lineString += this._terminal._core.buffer.translateBufferLineToString(lineIndex, !lineWrapsToNext && trimRight).substring(0, this._terminal.cols);
+            lineIndex++;
+        } while (lineWrapsToNext);
+        return lineString;
+    };
     SearchHelper.prototype._selectResult = function (result) {
         if (!result) {
+            this._terminal.clearSelection();
             return false;
         }
-        this._terminal.selectionManager.setSelection(result.col, result.row, result.term.length);
-        this._terminal.scrollDisp(result.row - this._terminal.buffer.ydisp, false);
+        this._terminal._core.selectionManager.setSelection(result.col, result.row, result.term.length);
+        this._terminal.scrollLines(result.row - this._terminal._core.buffer.ydisp);
         return true;
     };
     return SearchHelper;
 }());
 exports.SearchHelper = SearchHelper;
 
-
-
 },{}],2:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var SearchHelper_1 = require("./SearchHelper");
-(function (addon) {
-    if ('Terminal' in window) {
-        addon(window.Terminal);
+function findNext(terminal, term, searchOptions) {
+    if (searchOptions === void 0) { searchOptions = {}; }
+    var addonTerminal = terminal;
+    if (!addonTerminal.__searchHelper) {
+        addonTerminal.__searchHelper = new SearchHelper_1.SearchHelper(addonTerminal);
     }
-    else if (typeof exports === 'object' && typeof module === 'object') {
-        module.exports = addon(require('../../xterm'));
+    return addonTerminal.__searchHelper.findNext(term, searchOptions);
+}
+exports.findNext = findNext;
+function findPrevious(terminal, term, searchOptions) {
+    var addonTerminal = terminal;
+    if (!addonTerminal.__searchHelper) {
+        addonTerminal.__searchHelper = new SearchHelper_1.SearchHelper(addonTerminal);
     }
-    else if (typeof define == 'function') {
-        define(['../../xterm'], addon);
-    }
-})(function (Terminal) {
-    Terminal.prototype.findNext = function (term) {
-        if (!this._searchHelper) {
-            this.searchHelper = new SearchHelper_1.SearchHelper(this, Terminal.translateBufferLineToString);
-        }
-        return this.searchHelper.findNext(term);
+    return addonTerminal.__searchHelper.findPrevious(term, searchOptions);
+}
+exports.findPrevious = findPrevious;
+function apply(terminalConstructor) {
+    terminalConstructor.prototype.findNext = function (term, searchOptions) {
+        return findNext(this, term, searchOptions);
     };
-    Terminal.prototype.findPrevious = function (term) {
-        if (!this._searchHelper) {
-            this.searchHelper = new SearchHelper_1.SearchHelper(this, Terminal.translateBufferLineToString);
-        }
-        return this.searchHelper.findPrevious(term);
+    terminalConstructor.prototype.findPrevious = function (term, searchOptions) {
+        return findPrevious(this, term, searchOptions);
     };
+}
+exports.apply = apply;
+
+},{"./SearchHelper":1}]},{},[2])(2)
 });
-
-
-
-},{"../../xterm":undefined,"./SearchHelper":1}]},{},[2])
 //# sourceMappingURL=search.js.map
--- static/plugins/xterm/addons/terminado/terminado.js
+++ static/plugins/xterm/addons/terminado/terminado.js
@@ -1,138 +1,69 @@
-/**
- * This module provides methods for attaching a terminal to a terminado WebSocket stream.
- *
- * @module xterm/addons/terminado/terminado
- * @license MIT
- */
-
-(function (attach) {
-  if (typeof exports === 'object' && typeof module === 'object') {
-    /*
-     * CommonJS environment
-     */
-    module.exports = attach(require('../../xterm'));
-  } else if (typeof define == 'function') {
-    /*
-     * Require.js is available
-     */
-    define(['../../xterm'], attach);
-  } else {
-    /*
-     * Plain browser environment
-     */
-    attach(window.Terminal);
-  }
-})(function (Xterm) {
-  'use strict';
-
-  var exports = {};
-
-  /**
-   * Attaches the given terminal to the given socket.
-   *
-   * @param {Xterm} term - The terminal to be attached to the given socket.
-   * @param {WebSocket} socket - The socket to attach the current terminal.
-   * @param {boolean} bidirectional - Whether the terminal should send data
-   *                                  to the socket as well.
-   * @param {boolean} buffered - Whether the rendering of incoming data
-   *                             should happen instantly or at a maximum
-   *                             frequency of 1 rendering per 10ms.
-   */
-  exports.terminadoAttach = function (term, socket, bidirectional, buffered) {
-    bidirectional = (typeof bidirectional == 'undefined') ? true : bidirectional;
-    term.socket = socket;
-
-    term._flushBuffer = function () {
-      term.write(term._attachSocketBuffer);
-      term._attachSocketBuffer = null;
-      clearTimeout(term._attachSocketBufferTimer);
-      term._attachSocketBufferTimer = null;
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.terminado = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function terminadoAttach(term, socket, bidirectional, buffered) {
+    var addonTerminal = term;
+    bidirectional = (typeof bidirectional === 'undefined') ? true : bidirectional;
+    addonTerminal.__socket = socket;
+    addonTerminal.__flushBuffer = function () {
+        addonTerminal.write(addonTerminal.__attachSocketBuffer);
+        addonTerminal.__attachSocketBuffer = null;
     };
-
-    term._pushToBuffer = function (data) {
-      if (term._attachSocketBuffer) {
-        term._attachSocketBuffer += data;
-      } else {
-        term._attachSocketBuffer = data;
-        setTimeout(term._flushBuffer, 10);
-      }
-    };
-
-    term._getMessage = function (ev) {
-      var data = JSON.parse(ev.data)
-      console.log(data);
-      term.write(data[1]);
-      if( data[0] == "stdout" ) {
-        if (buffered) {
-          term._pushToBuffer(data[1]);
-        } else {
-          term.write(data[1]);
+    addonTerminal.__pushToBuffer = function (data) {
+        if (addonTerminal.__attachSocketBuffer) {
+            addonTerminal.__attachSocketBuffer += data;
         }
-      }
+        else {
+            addonTerminal.__attachSocketBuffer = data;
+            setTimeout(addonTerminal.__flushBuffer, 10);
+        }
     };
-
-    term._sendData = function (data) {
-      console.log(data);
-      socket.send(JSON.stringify(['stdin', data]));
+    addonTerminal.__getMessage = function (ev) {
+        var data = JSON.parse(ev.data);
+        if (data[0] === 'stdout') {
+            if (buffered) {
+                addonTerminal.__pushToBuffer(data[1]);
+            }
+            else {
+                addonTerminal.write(data[1]);
+            }
+        }
     };
-
-    term._setSize = function (size) {
-      socket.send(JSON.stringify(['set_size', size.rows, size.cols]));
+    addonTerminal.__sendData = function (data) {
+        socket.send(JSON.stringify(['stdin', data]));
     };
-
-    socket.addEventListener('message', term._getMessage);
-
+    addonTerminal.__setSize = function (size) {
+        socket.send(JSON.stringify(['set_size', size.rows, size.cols]));
+    };
+    socket.addEventListener('message', addonTerminal.__getMessage);
     if (bidirectional) {
-      term.on('data', term._sendData);
+        addonTerminal.on('data', addonTerminal.__sendData);
     }
-    term.on('resize', term._setSize);
-
-    socket.addEventListener('close', term.terminadoDetach.bind(term, socket));
-    socket.addEventListener('error', term.terminadoDetach.bind(term, socket));
-  };
-
-  /**
-   * Detaches the given terminal from the given socket
-   *
-   * @param {Xterm} term - The terminal to be detached from the given socket.
-   * @param {WebSocket} socket - The socket from which to detach the current
-   *                             terminal.
-   */
-  exports.terminadoDetach = function (term, socket) {
-    term.off('data', term._sendData);
-
-    socket = (typeof socket == 'undefined') ? term.socket : socket;
-
+    addonTerminal.on('resize', addonTerminal.__setSize);
+    socket.addEventListener('close', function () { return terminadoDetach(addonTerminal, socket); });
+    socket.addEventListener('error', function () { return terminadoDetach(addonTerminal, socket); });
+}
+exports.terminadoAttach = terminadoAttach;
+function terminadoDetach(term, socket) {
+    var addonTerminal = term;
+    addonTerminal.off('data', addonTerminal.__sendData);
+    socket = (typeof socket === 'undefined') ? addonTerminal.__socket : socket;
     if (socket) {
-      socket.removeEventListener('message', term._getMessage);
+        socket.removeEventListener('message', addonTerminal.__getMessage);
     }
+    delete addonTerminal.__socket;
+}
+exports.terminadoDetach = terminadoDetach;
+function apply(terminalConstructor) {
+    terminalConstructor.prototype.terminadoAttach = function (socket, bidirectional, buffered) {
+        return terminadoAttach(this, socket, bidirectional, buffered);
+    };
+    terminalConstructor.prototype.terminadoDetach = function (socket) {
+        return terminadoDetach(this, socket);
+    };
+}
+exports.apply = apply;
 
-    delete term.socket;
-  };
-
-  /**
-   * Attaches the current terminal to the given socket
-   *
-   * @param {WebSocket} socket - The socket to attach the current terminal.
-   * @param {boolean} bidirectional - Whether the terminal should send data
-   *                                  to the socket as well.
-   * @param {boolean} buffered - Whether the rendering of incoming data
-   *                             should happen instantly or at a maximum
-   *                             frequency of 1 rendering per 10ms.
-   */
-  Xterm.prototype.terminadoAttach = function (socket, bidirectional, buffered) {
-    return exports.terminadoAttach(this, socket, bidirectional, buffered);
-  };
-
-  /**
-   * Detaches the current terminal from the given socket.
-   *
-   * @param {WebSocket} socket - The socket from which to detach the current
-   *                             terminal.
-   */
-  Xterm.prototype.terminadoDetach = function (socket) {
-    return exports.terminadoDetach(this, socket);
-  };
-
-  return exports;
+},{}]},{},[1])(1)
 });
+//# sourceMappingURL=terminado.js.map
