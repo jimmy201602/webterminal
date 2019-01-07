bdd8935e7fa71fc23d7cddbf4ab86daa3617323d
jimmy201602 <zhengge2012@gmail.com>
2019-01-07 21:14:13 +0800

    pep8 format

--- webterminal/routing.py
+++ webterminal/routing.py
@@ -1,5 +1,5 @@
 from channels import route_class, route
-from webterminal.consumers import webterminal, CommandExecute, SshTerminalMonitor, BatchCommandExecute
+from webterminal.consumers import Webterminal, CommandExecute, SshTerminalMonitor, BatchCommandExecute
 from guacamole.consumers import GuacamoleWebsocket, GuacamoleMonitor
 
 # The channel routing defines what channels get handled by what consumers,
@@ -7,7 +7,7 @@ from guacamole.consumers import GuacamoleWebsocket, GuacamoleMonitor
 # all WebSocket connections to the class-based BindingConsumer (the consumer
 # class itself specifies what channels it wants to consume)
 channel_routing = [
-    route_class(webterminal, path=r'^/ws'),
+    route_class(Webterminal, path=r'^/ws'),
     route_class(CommandExecute, path=r'^/execute'),
     route_class(SshTerminalMonitor, path=r'^/monitor/(?P<channel>[\w-]+)'),
     route_class(GuacamoleWebsocket, path=r'^/guacamole/(?P<id>\w+)/'),
