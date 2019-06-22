b04e94a877648c94c7db1c7d7851fec8a1d4136a
jimmy201602 <zhengge2012@gmail.com>
2019-06-22 17:30:21 +0800

    update vue version

--- fronted/src/router/index.js
+++ fronted/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from 'components/Hello.vue'
+import Hello from '../components/Hello.vue'
 import LayOut from '../LayOut.vue'
 
 Vue.use(Router)
