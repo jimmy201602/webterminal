04c8740d105415c12c76e7d1a560a90acc3dbf04
jimmy201602 <zhengge2012@gmail.com>
2019-06-22 18:55:37 +0800

    add footer

---
+++ fronted/src/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+            <b>{{ $t('version') }} 0.1</b>
+        </div>
+        <strong>{{ $t('Copyright') }}  &copy; 2014-2019 <a href="https://github.com/jimmy201602/webterminal">{{ $t('webterminal') }}</a></strong> {{ $t('All rights reserved.') }}
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
