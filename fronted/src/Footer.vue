8444d63b2e5ffe4c85dcab975bbb2e587a57b47f
jimmy201602 <zhengge2012@gmail.com>
2019-06-22 23:56:33 +0800

    add locale translation

--- fronted/src/Footer.vue
+++ fronted/src/Footer.vue
@@ -3,7 +3,7 @@
         <div class="pull-right hidden-xs">
             <b>{{ $t('version') }} 0.1</b>
         </div>
-        <strong>{{ $t('Copyright') }}  &copy; 2014-2019 <a href="https://github.com/jimmy201602/webterminal">{{ $t('webterminal') }}</a></strong> {{ $t('All rights reserved.') }}
+        <strong>{{ $t('Copyright') }}  &copy; 2014-2019 <a href="https://github.com/jimmy201602/webterminal">webterminal</a></strong> {{ $t('All rights reserved') }}.
     </footer>
 </template>
 
