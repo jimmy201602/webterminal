53ada328e2202bdfe053b4ad6cf7f5ed9a37902f
jimmy201602 <zhengge2012@gmail.com>
2019-06-22 17:49:41 +0800

    modify adminlte

--- fronted/src/lib/slideMenuItems.js
+++ fronted/src/lib/slideMenuItems.js
@@ -12,73 +12,9 @@ module.exports = [
       {
         type: 'item',
         icon: 'fa fa-circle-o',
-        name: '헬로화면',
-        router: {
-          name: 'Hello'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: '샘플화면',
-        router: {
-          name: 'Sample'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
         name: 'Dashboard v1',
         router: {
           name: 'DashboardV1'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Dashboard v2',
-        router: {
-          name: 'DashboardV2'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'InfoBox',
-        router: {
-          name: 'InfoBoxExample'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Chart.js',
-        router: {
-          name: 'ChartExample'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Alert',
-        router: {
-          name: 'AlertExample'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Modal',
-        router: {
-          name: 'ModalExample'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'APIExample',
-        router: {
-          name: 'APIExample'
         }
       }
     ]
@@ -97,24 +33,6 @@ module.exports = [
         icon: 'fa fa-circle-o',
         name: 'Top Navigation',
         link: 'https://adminlte.io/themes/AdminLTE/pages/layout/top-nav.html'
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Boxed',
-        link: 'https://adminlte.io/themes/AdminLTE/pages/layout/boxed.html'
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Fixed',
-        link: 'https://adminlte.io/themes/AdminLTE/pages/layout/fixed.html'
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Collapsed Sidebar',
-        link: 'https://adminlte.io/themes/AdminLTE/pages/layout/collapsed-sidebar.html'
       }
     ]
   },
@@ -142,46 +60,6 @@ module.exports = [
         router: {
           name: 'General'
         }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Icons',
-        router: {
-          name: 'Icons'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Buttons',
-        router: {
-          name: 'Buttons'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Sliders',
-        router: {
-          name: 'Sliders'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Timeline',
-        router: {
-          name: 'Timeline'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Modals',
-        router: {
-          name: 'Modals'
-        }
       }
     ]
   },
@@ -197,20 +75,6 @@ module.exports = [
         router: {
           name: 'GeneralElements'
         }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Advanced Elements',
-        router: {
-          name: 'AdvancedElements'
-        }
-      },
-      {
-        type: 'item',
-        icon: 'fa fa-circle-o',
-        name: 'Editors',
-        link: 'pages/forms/editors.html'
       }
     ]
   }
