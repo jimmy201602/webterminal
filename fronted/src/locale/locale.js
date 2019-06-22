80f42940b0da29a074a2397c592773dbe17c1537
jimmy201602 <zhengge2012@gmail.com>
2019-06-22 18:03:49 +0800

    add locale file

---
+++ fronted/src/locale/locale.js
@@ -0,0 +1,12 @@
+export default {
+    'zh-CN': {
+        test: {
+            test: '测试!'
+        }
+    },
+    'en-US': {
+        index: {
+            test: 'This is a test!'
+        }
+    }
+}
