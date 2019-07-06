c7525079b8d459917f58928d2ef2296dde0e7d93
jimmy201602 <zhengge2012@gmail.com>
2019-07-06 13:45:15 +0800

    add close chan function

--- webterminal/interactive.py
+++ webterminal/interactive.py
@@ -80,6 +80,7 @@ def posix_shell(chan, channel, log_name=None, width=90, height=40, elementid=Non
                     data = chan.recv(1024)
                     x = u(data)
                     if x == '<<<close>>>':  # close flag
+                        chan.close()
                         logger.debug('close ssh session')
                         break
                     if len(x) == 0:
