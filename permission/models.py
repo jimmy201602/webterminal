817e40e6d29788c327355d271ab9bd2484abc367
jimmy201602 <zhengge2012@gmail.com>
2019-08-17 14:53:38 +0800

    add role permission control

--- permission/models.py
+++ permission/models.py
@@ -38,3 +38,34 @@ class Permission(models.Model):
             ("can_delete_permissions", _("Can revoke user permissions")),
             ("can_add_permissions", _("Can add user permissions")),
         )
+
+
+class Role(models.Model):
+    user = models.OneToOneField(User, verbose_name=_(
+        'User'), related_name='roleuser')
+    permissions = models.ManyToManyField(
+        AuthPermission, verbose_name=_('Permissions'), related_name='rolepermission', limit_choices_to={'content_type__app_label__in': ['common', 'permission'], "codename__contains": 'can_'})
+    groups = models.ManyToManyField(
+        ServerGroup, verbose_name=_('Server group'))
+    createdatetime = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('Create time'))
+    updatedatetime = models.DateTimeField(
+        auto_created=True, auto_now=True, verbose_name=_('Update time'))
+
+    def __unicode__(self):
+        return self.user.username
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        permissions = (
+            ("can_add_user", _("Can add role")),
+            ("can_change_user", _("Can change role info")),
+            ("can_delete_user", _("Can delete role info")),
+            ("can_view_user", _("Can view role info")),
+            ("can_view_permissions", _("Can view role permissions")),
+            ("can_change_permissions", _("Can change role permissions")),
+            ("can_delete_permissions", _("Can revoke role permissions")),
+            ("can_add_permissions", _("Can add role permissions")),
+        )
