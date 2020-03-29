6b881bd457e9e4db0c7385a6f4b3145c9ed8d3ca
jimmy201602 <zhengge2012@gmail.com>
2020-03-29 15:46:25 +0800

    pep8 format

--- plugins/client.py
+++ plugins/client.py
@@ -1,11 +1,12 @@
 import docker
 
+
 class DockerBridgeClient(object):
     def __init__(self):
         self.client = docker.APIClient()
-    
+
     def get_all_running_cotainers(self):
         return self.client.containers()
-    
+
     def get_all_images(self):
         return self.client.images()
