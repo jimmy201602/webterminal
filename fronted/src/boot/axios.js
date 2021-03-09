4c986c44c8ee18c01fc5b05856b40cf9b35ecb76
jimmy201602 <zhengge2012@gmail.com>
2021-03-09 12:53:48 +0800

    remove fronted

--- fronted/src/boot/axios.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import axios from 'axios'
-
-Vue.prototype.$axios = axios
--- fronted/src/store/modules/state.js
+++
@@ -1,9 +0,0 @@
-export default function () {
-  return {
-    userInfo: {
-      username: '',
-      avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
-      role: 'Developer'
-    }
-  }
-}
