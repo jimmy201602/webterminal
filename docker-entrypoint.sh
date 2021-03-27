0846b554fe66dc4d1fca0eb916371ac286978900
jimmy201602 <zhengge2012@gmail.com>
2021-03-27 14:04:49 +0800

    split the db to individual directory

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,12 @@ set -e
 trap "kill -15 -1 && echo all proc killed" TERM KILL INT
 
 if [ "$1" = "start" ]; then
+	if [ ! -f /opt/webterminal/db/db.sqlite3 ]; then
+		cd /opt/webterminal
+		python3 manage.py makemigrations
+		python3 manage.py migrate
+		python3 createsuperuser.py
+	fi
 	service nginx start
 	/usr/local/bin/supervisord -c /etc/supervisor/supervisord.conf
 	sleep inf & wait
--- webterminal/settings.py
+++ webterminal/settings.py
@@ -103,7 +103,7 @@ ASGI_APPLICATION = "webterminal.routing.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
