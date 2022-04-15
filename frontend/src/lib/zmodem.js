5ff93fdc2a647c2a0fee5f758234a6bcdc172520
zhengge2012 <zhengge2012@gmail.com>
2022-04-15 12:10:37 +0800

    add zmodem js

---
+++ frontend/src/lib/zmodem.js
@@ -0,0 +1,51 @@
+// this js from xterm (version 3) addons
+// https://github.com/xtermjs/xterm.js/blob/3.14.5/src/addons/zmodem/zmodem.ts
+import zmodem from 'zmodem.js/src/zmodem_browser'
+
+function zmodemAttach (ws, opts, ctx) {
+  // eslint-disable-next-line no-void
+  if (opts === void 0) { opts = {} }
+  const term = this
+  const senderFunc = function (octets) { return ws.send(new Uint8Array(octets)) }
+  let zsentry = null
+  function shouldWrite () {
+    return !!zsentry.get_confirmed_session() || !opts.noTerminalWriteOutsideSession
+  }
+  zsentry = new zmodem.Sentry({
+    to_terminal: function (octets) {
+      if (shouldWrite()) {
+        term.write(String.fromCharCode.apply(String, octets))
+      }
+    },
+    sender: senderFunc,
+    on_retract: ctx.onzmodemRetract,
+    on_detect: ctx.onZmodemDetect
+  })
+  function handleWSMessage (evt) {
+    if (typeof evt.data === 'string') {
+      if (shouldWrite()) {
+        term.write(evt.data)
+      }
+    } else {
+      zsentry.consume(evt.data)
+    }
+  }
+  ws.binaryType = 'arraybuffer'
+  ws.addEventListener('message', handleWSMessage)
+}
+
+export class AddonZmodem {
+  _disposables = []
+
+  activate (terminal) {
+    terminal.zmodemAttach = zmodemAttach
+    terminal.zmodemBrowser = zmodem.Browser
+  }
+
+  dispose () {
+    this._disposables.forEach(d => d.dispose())
+    this._disposables.length = 0
+  }
+}
+
+export const Zmodem = zmodem
